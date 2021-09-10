259d4ac97a2c48d09974496c37585ceab76db1b1
Stefan Götz <github.nooneelse@spamgourmet.com>
2021-09-10 23:11:15 +1000

    - infrastructure for python

---
+++ initbot/main.py
@@ -0,0 +1,6 @@
+def _main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    _main()
---
+++ tools/precommit.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Copyright 2021 Stefan Götz <github.nooneelse@spamgourmet.com>
+
+# This file is part of initbot.
+
+# initbot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+
+# initbot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public
+# License along with initbot. If not, see <https://www.gnu.org/licenses/>.
+
+set -ue
+
+cd "$(dirname "$(realpath "${0}")")"/..
+
+poetry run pre-commit run --all-files
