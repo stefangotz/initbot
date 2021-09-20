8b0e8bf6940874c04f5717c02f166641376f603d
Stefan Götz <stg@brkawy.com>
2021-09-20 20:00:13 +1000

    - use commands - split into separate files

---
+++ initbot/bot.py
@@ -0,0 +1,16 @@
+from discord.ext.commands import Bot
+
+from .config import CFG
+from .commands.roll import roll
+
+bot = Bot(command_prefix="$")
+
+
+@bot.event
+async def on_ready():
+    print("Logged in as {0.user}".format(bot))
+
+
+def run():
+    bot.add_command(roll)
+    bot.run(CFG.token)
---
+++ initbot/commands/roll.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+import random
+import re
+
+from discord.ext import commands
+
+_DIE_PATTERN = re.compile(r"^([0-9]*)d([0-9]+)([+-][0-9]+)?$", re.IGNORECASE)
+
+
+@dataclass
+class DieRoll:
+    sides: int
+    dice: int = 1
+    modifier: int = 0
+
+    def __init__(self, text: str):
+        match = _DIE_PATTERN.match(text)
+        if match:
+            self.sides = int(match.group(2))
+            if match.group(1):
+                self.dice = int(match.group(1))
+            if match.group(3):
+                self.modifier = int(match.group(3))
+
+    def roll(self):
+        return self.dice * random.randint(1, self.sides) + self.modifier
+
+    @staticmethod
+    def is_die_roll(text: str) -> bool:
+        return bool(_DIE_PATTERN.match(text))
+
+
+@commands.command()
+async def roll(ctx, txt: str):
+    if DieRoll.is_die_roll(txt):
+        result = DieRoll(txt).roll()
+        await ctx.send(
+            f"You rolled **{result}** (_{ctx.message.content}_ - {ctx.author.display_name})"
+        )
