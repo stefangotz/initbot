b1701c25556836cf73839c5d3a6146a4226a001b
Stefan Götz <github.nooneelse@spamgourmet.com>
2021-10-15 20:30:15 +1000

    - fix typo in $roll message

--- initbot/commands/roll.py
+++ initbot/commands/roll.py
@@ -71,7 +71,7 @@ async def roll(ctx, txt: str):
             string = str(roll_result[0])
         else:
             string = str(roll_result).strip("[]")
-        await ctx.send(f"{ctx.author.display_name} rolled **{string}** on {dice})")
+        await ctx.send(f"{ctx.author.display_name} rolled **{string}** on {dice}")
 
 
 @roll.error
