db4a72511754162803b1f3e59ef4416926fcbcc0
Andrea James <roceal@jngm.net>
2021-11-10 23:10:14 +1100

    Refactors and tweaks the $sound command in preparation for creating a list of sounds on the soundboard

--- initbot/bot/commands/soundboard.py
+++ initbot/bot/commands/soundboard.py
@@ -1,16 +1,20 @@
 from discord.ext import commands  # type: ignore
 from discord.player import FFmpegPCMAudio  # type: ignore
+from discord import VoiceChannel  # type: ignore
 
 
-@commands.command()
-async def sound(ctx):
+async def play_sound(voice_channel: VoiceChannel):
+    client = await voice_channel.connect()
+    audio = FFmpegPCMAudio(source="./initbot/sounds/battle-160.mp3")
+    client.play(audio)
+
+
+@commands.command(usage="[sound name]")
+async def sound(ctx, sound_name: str):
     """Plays a sound."""
     if len(ctx.guild.voice_channels) > 0:
-        voice_channel = ctx.guild.voice_channels[0]
-        client = await voice_channel.connect()
-        await ctx.send("Playing battle...", delete_after=10)
-        audio = FFmpegPCMAudio(source="./initbot/sounds/battle-160.mp3")
-        client.play(audio)
+        await ctx.send(f":loudspeaker: Playing {sound_name}", delete_after=10)
+        await play_sound(ctx.guild.voice_channels[0])
 
 
 @sound.error
