d8f1eaf5aeecc69968295d1d271f1eb875e452e6
Stefan Götz <github.nooneelse@spamgourmet.com>
2021-11-25 20:33:44 +1000

    - load birth augurs via pydantic helper function - improve formatting of birth augurs in Discord

--- initbot/bot/commands/augur.py
+++ initbot/bot/commands/augur.py
@@ -1,27 +1,25 @@
 from pathlib import Path
 from typing import List, Dict
-import json
 import logging
 import random
 
 from discord.ext import commands  # type: ignore
 
-from ...data.augur import AugurData
+from ...data.augur import AugurData, AugursData
 
 
-_AUGURS: List[AugurData] = []
+_AUGURS_DATA = AugursData(augurs=[])
 _PATH: Path = Path(__file__).parent / "augurs.json"
 if _PATH.exists():
-    with open(_PATH, encoding="utf8") as fd:
-        _AUGURS = [AugurData(**a) for a in json.load(fd)["augurs"]]  # type: ignore
+    _AUGURS_DATA = AugursData.parse_file(_PATH)
 else:
     logging.warning("Unable to find %s", _PATH)
 
-_AUGURS_DICT: Dict[int, AugurData] = {aug.roll: aug for aug in _AUGURS}
+_AUGURS_DICT: Dict[int, AugurData] = {aug.roll: aug for aug in _AUGURS_DATA.augurs}
 
 
 def get_augurs() -> List[AugurData]:
-    return _AUGURS
+    return _AUGURS_DATA.augurs
 
 
 def get_augur(roll: int) -> AugurData:
@@ -30,12 +28,21 @@ def get_augur(roll: int) -> AugurData:
 
 @commands.command()
 async def augurs(ctx):
-    await ctx.send(str(_AUGURS))
+    """List all birth augurs that a 0-level character may start out with."""
+    msg: str = "*Birth augurs modify certain character properties by the initial starting luck modifier of the character.*\n"
+    for agr in _AUGURS_DATA.augurs:
+        txt: str = f"{agr.roll}: {agr.description}\n"
+        if len(msg) + len(txt) > 2000:
+            await ctx.send(msg.rstrip())
+            msg = ""
+        msg += txt
+    await ctx.send(msg.rstrip())
 
 
 @commands.command()
 async def augur(ctx):
-    await ctx.send(str(random.choice(_AUGURS)))
+    """Display a randomly chosen birth augur."""
+    await ctx.send(str(random.choice(_AUGURS_DATA.augurs)))
 
 
 @augurs.error
--- initbot/data/augur.py
+++ initbot/data/augur.py
@@ -1,6 +1,12 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
 class AugurData(BaseModel):
     description: str
     roll: int
+
+
+class AugursData(BaseModel):
+    augurs: List[AugurData]
