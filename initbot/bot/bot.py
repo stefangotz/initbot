9a69be522e0c6fd9975cceb302dfc7e2017ded87
Stefan Götz <github.nooneelse@spamgourmet.com>
2023-09-01 22:29:18 +1000

    - upgrade to discord.py 2

--- initbot/bot/bot.py
+++ initbot/bot/bot.py
@@ -1,10 +1,14 @@
-from discord.ext.commands import Bot  # type: ignore
+from discord.ext.commands import Bot
+from discord import Intents
 
 from .config import CFG
 from .commands import commands
 from ..state.local import LocalState
 
-bot = Bot(command_prefix="$")
+intents = Intents.default()
+intents.message_content = True
+
+bot = Bot(command_prefix="$", intents=intents)
 
 
 @bot.event
@@ -15,5 +19,5 @@ async def on_ready():
 def run():
     for cmd in commands:
         bot.add_command(cmd)
-    bot.initbot_state = LocalState()
+    bot.initbot_state = LocalState()  # type: ignore
     bot.run(CFG.token)
